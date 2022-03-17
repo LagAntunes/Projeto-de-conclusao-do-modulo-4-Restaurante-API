fef5eb3b20bff263cb1d510cf9a9de6890cab4c9
LagAntunes <lag151617@gmail.com>
2022-03-17 20:14:52 -0300

    adicionando as extensões sqlite e sqlite3 ao projeto

--- src/model/ingredientes.js
+++ src/model/ingredientes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const ingredientes = express();
+ingredientes.use(express.json())
 
 ingredientes.get('/', function(req, res) {
-    res.send("Olá mundo")
-})
+    res.send("Teste de conexão")
+});
+
+ingredientes.post('/ingredientes', function(req, res) {
+    console.log(req.body)
+    res.json({
+        "statusCode": 200
+    })
+});
 
 ingredientes.listen(3000, () => console.log("Api rodando"))
