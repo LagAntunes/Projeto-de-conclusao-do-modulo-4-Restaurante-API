6bec7bfd90ff70a370ff017d50025a0748df70cd
LagAntunes <lag151617@gmail.com>
2022-03-17 22:08:27 -0300

    implementando operações de CRUD

--- src/controler/tabelasIngredientes.js
+++ src/controler/tabelasIngredientes.js
@@ -5,3 +5,36 @@ export async function createTable() {
         db.exec('CREATE TABLE IF NOT EXISTS Ingredientes (id INTEGER PRIMARY KEY, nome VARCHAR(50), tipo VARCHAR(50), valorKG DECIMAL(10,2) )')
     })
 }
+
+export async function insertIngrediente(ingrediente) {
+    openDb().then(db => {
+        db.run('INSERT INTO Ingredientes (nome, tipo, valorKG) VALUES (?, ?, ?)', [ingrediente.nome, ingrediente.tipo, ingrediente.valorKG])
+    })
+}
+
+export async function updateIngrediente(ingrediente) {
+    openDb().then(db => {
+        db.run('UPDATE Ingredientes SET nome=?, tipo=?, valorKG=? WHERE id=?', [ingrediente.nome, ingrediente.tipo, ingrediente.valorKG, ingrediente.id])
+    })
+}
+
+export async function selectIngrediente() {
+    openDb().then(db => {
+        return db.all('SELECT * FROM ingrediente')
+        .then(res => res)
+    })
+}
+
+export async function selectUnicoIngrediente(id) {
+    openDb().then(db => {
+        return db.get('SELECT * FROM ingrediente WHERE id=?', [id])
+        .then(res => res)
+    })
+}
+
+export async function deleteIngrediente(id) {
+    openDb().then(db => {
+        return db.get('DELETE FROM ingrediente WHERE id=?', [id])
+        .then(res => res)
+    })
+}
--- src/model/ingredientes.js
+++ src/model/ingredientes.js
@@ -1,5 +1,5 @@
 import {openDb} from '../configDB.js';
-import {createTable} from '../controler/tabelasIngredientes.js';
+import {createTable, insertIngrediente, updateIngrediente, selectIngrediente, selectUnicoIngrediente, deleteIngrediente} from '../controler/tabelasIngredientes.js';
 
 import express from 'express';
 const ingredientes = express();
@@ -11,11 +11,40 @@ ingredientes.get('/', function(req, res) {
     res.send("Teste de conexão")
 });
 
+ingredientes.get('/ingredientes', async function(req, res) {
+    let ingredientesSelecionados = await selectIngrediente();
+    res.json(ingredientesSelecionados);
+});
+
+ingredientes.get('/ingredienteUnico', async function(req, res) {
+    let ingredienteSelecionado = await selectUnicoIngrediente(req.body.id);
+    res.json(ingredienteSelecionado);
+});
+
 ingredientes.post('/ingredientes', function(req, res) {
-    console.log(req.body)
+    insertIngrediente(req.body)
     res.json({
         "statusCode": 200
     })
+});
+
+ingredientes.put('/ingredientes', function(req, res) {
+    if(req.body && !req.body.id) {
+        res.json({
+            "statusCode": 400,
+            "msg": "Informe um Id válido"
+        })
+    } else {
+        updateIngrediente(req.body)
+        res.json({
+            "statusCode": 200
+        })
+    }
+});
+
+ingredientes.delete('/ingredientes', async function(req, res) {
+    let ingredienteSelecionado = await deleteIngrediente(req.body.id);
+    res.json(ingredienteSelecionado);
 });
 
 ingredientes.listen(3000, () => console.log("Api rodando"))
